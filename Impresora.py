483b99f6900335fa796176173150d091a0bee41e
noelb <arci.daw2v@gmail.com>
2023-02-23 13:04:29 +0100

    Subimos la impresora a el repositorio con una modificacion

--- Impresora.py
+++ Impresora.py
@@ -4,6 +4,7 @@ variable3 = 3
 variable4 = 4
 
 print(f"{variable1},{variable2},{variable3},{variable4}")
+print("Hola?")
 
 lista_variables = [variable1, variable2, variable3, variable4]
 for posicion in range(len(lista_variables)):
