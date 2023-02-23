06ce55bc5f777e8b08077d7fb03623eecad2962a
TeamGoEs <126160927+TeamGoEs@users.noreply.github.com>
2023-02-23 12:59:30 +0100

    Update Impresora.py

--- Impresora.py
+++ Impresora.py
@@ -4,3 +4,7 @@ variable3 = 3
 variable4 = 4
 
 print(f"{variable1},{variable2},{variable3},{variable4}")
+
+lista_variables = [variable1, variable2, variable3, variable4]
+for posicion in range(len(lista_variables)):
+    print(lista_variables[posicion])
